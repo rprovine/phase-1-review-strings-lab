d560009bce29ec977a608a85f9aeb7718b1398fc
Reno <rprovine@gmail.com>
2023-07-31 09:33:26 -0700

    passing

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Reno Provine';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
